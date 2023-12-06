a997d1c796d234c2b9a45e80088848e622df9a20
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-12-06 12:28:43 +0900

    feat :: service setup

--- src/main/java/com/grpc/server/service/MyServiceImpl.java
+++ src/main/java/com/grpc/server/service/MyServiceImpl.java
@@ -4,10 +4,12 @@ import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.chb.examples.lib.HelloReply;
 import org.chb.examples.lib.HelloRequest;
+import org.chb.examples.lib.SimpleGrpc;
 
 @GrpcService
-public class MyServiceImpl {
+public class MyServiceImpl extends SimpleGrpc.SimpleImplBase{
 
+    @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> response) {
         HelloReply reply = HelloReply.newBuilder()
                 .setMessage("Hello ==> " + request.getName())
